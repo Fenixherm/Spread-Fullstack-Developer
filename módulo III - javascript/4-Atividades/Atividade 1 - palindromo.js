bccccf79b7416be1025676212c346f775f66a5b4
Fenixherm <guibah78@gmail.com>
2023-06-13 13:21:34 -0300

    Segunda solução do exercicio + soluçao da professora

--- módulo III - javascript/4-Atividades/Atividade 1 - palindromo.js
+++ módulo III - javascript/4-Atividades/Atividade 1 - palindromo.js
@@ -5,10 +5,53 @@ function verificaPalindromo(palavra) {
     let palindromo = palavra.split("").reverse().join("")
 
     if(palavra !== palindromo){
-        return "Não é um palíndromo"
+        return false
     }
  
-    return "É um palíndromo"
+    return true
 }
 
-console.log(verificaPalindromo("araro"))
+function solucao1Aula (palavra){
+    if(!palavra) return "Defina a palavra"
+
+    return palavra.split("").reverse().join("") === palavra;
+}
+
+function solucao2 (palavra) {
+    if(!palavra) return "Defina a palavra"
+
+    palavra = palavra.replace(/ /g, '')
+    let palindromo = palavra.split("").reverse().join("")
+
+    for(let i = 0; i < palavra.length; i++){
+        if(palindromo[i] !== palavra[i]) return false
+    }
+    
+    return true
+}
+
+/*
+solução 2
+ fazer duas checagem por loop ou seja divide por 2 o length
+ nesse caso pegamos o length - 1 (para pegar o índice correto, pois começa do zero)
+ e tiramos -i, pois a cada loop ele precisa verificar o anterior
+
+ omo
+ 012
+ abba
+ 0123
+*/
+
+function solucao2Aula (palavra) {
+    if(!palavra) return "Defina a palavra"
+
+    for(let i = 0; i < palavra.length / 2; i++){
+        if(palavra[i] !== palavra[palavra.length -1 - i]){
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log(solucao2Aula("arara"))
