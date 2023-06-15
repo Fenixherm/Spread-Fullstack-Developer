0b1f9fa5c01422245c2daeac32753be68fccf01f
Fenixherm <guibah78@gmail.com>
2023-06-15 13:05:35 -0300

    atividade incompleta

---
+++ módulo III - javascript/5 - This/atividade1.js
@@ -0,0 +1,35 @@
+function mediaSala(arr, media) {
+
+    let arrayAuxiliar = []
+
+}
+
+const alunos = 
+[
+    {
+        nome: "Jonas",
+        nota: 8,
+        turma: "1B",
+    },
+
+    {
+        nome: "Lucas",
+        nota: 7,
+        turma: "1B",
+    },
+
+    {
+        nome: "Kevin",
+        nota: 6,
+        turma: "1B",
+    },
+
+    {
+        nome: "Guilherme",
+        nota: 4,
+        turma: "2B",
+    },
+]
+
+
+mediaSala(alunos)
