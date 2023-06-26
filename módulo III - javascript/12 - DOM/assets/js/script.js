df61499952224dd059c556818b8860bde58cff62
Fenixherm <guibah78@gmail.com>
2023-06-26 13:44:39 -0300

    arrumado o height do body, -arrumar footer

--- módulo III - javascript/12 - DOM/assets/js/script.js
+++ módulo III - javascript/12 - DOM/assets/js/script.js
@@ -1,18 +1,16 @@
 const botao = document.getElementsByClassName("btn-dark")[0];
-const body = document.getElementsByClassName("corpo")[0];
+const body = document.getElementsByTagName("body")[0];
 let titulo = document.getElementsByClassName("titulo")[0];
-let texto = document.getElementsByClassName("titulo")[0];
 
 
 botao.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
+
     titulo.classList.toggle("titulo-branco");
 
-    if(texto.textContent === ""){
-        texto.textContent = "Light Mode: OFF";
-    }else if(texto.textContent === "Light Mode: OFF"){
-        texto.textContent = "Light Mode: ON";
-    }else{
-        texto.textContent = "Light Mode: OFF";
+    if(titulo.textContent !== "Light Mode: OFF"){
+        titulo.textContent = "Light Mode: OFF";
+    }else if(titulo.textContent === "Light Mode: OFF"){
+        titulo.textContent = "Light Mode: ON";
     }
 });
