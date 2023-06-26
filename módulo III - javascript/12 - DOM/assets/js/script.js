564bf7b8c1dc0159d4258f0c92bd451e1423b1ff
Fenixherm <guibah78@gmail.com>
2023-06-26 13:09:55 -0300

    Botao funcionando, falta arrumar o footer!

--- módulo III - javascript/12 - DOM/assets/js/script.js
+++ módulo III - javascript/12 - DOM/assets/js/script.js
@@ -1,10 +1,18 @@
 const botao = document.getElementsByClassName("btn-dark")[0];
-const body = document.getElementById("corpo");
+const body = document.getElementsByClassName("corpo")[0];
 let titulo = document.getElementsByClassName("titulo")[0];
+let texto = document.getElementsByClassName("titulo")[0];
 
 
 botao.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
     titulo.classList.toggle("titulo-branco");
-    document.getElementsByClassName("titulo")[0].textContent = "Light Mode: OFF";
+
+    if(texto.textContent === ""){
+        texto.textContent = "Light Mode: OFF";
+    }else if(texto.textContent === "Light Mode: OFF"){
+        texto.textContent = "Light Mode: ON";
+    }else{
+        texto.textContent = "Light Mode: OFF";
+    }
 });
