8a5e6b6f63d7502fcf863ca9051de4439904b974
Fenixherm <guibah78@gmail.com>
2023-06-26 20:55:30 -0300

    Solucao do desafio de DOM 'Dark Mode'

--- módulo III - javascript/12 - DOM/assets/js/script.js
+++ módulo III - javascript/12 - DOM/assets/js/script.js
@@ -1,16 +1,44 @@
-const botao = document.getElementsByClassName("btn-dark")[0];
-const body = document.getElementsByTagName("body")[0];
-let titulo = document.getElementsByClassName("titulo")[0];
+/* minha solução */
 
+const botao = document.getElementById("btn");
+const body = document.getElementsByTagName("body")[0]; //precisa do índice pois é retornado uma coleção.
+const titulo = document.getElementById("titulo");
+const footer = document.getElementById("text-footer");
 
-botao.addEventListener("click", () => {
+botao.addEventListener("click", changeMode);
+
+function changeMode(){
+    changeClasses();
+    changeStyle();
+    changeText();
+}
+
+function changeClasses(){
     body.classList.toggle("dark-mode");
+    botao.classList.toggle("dark-mode");
+    titulo.classList.toggle("dark-mode");
+}
 
-    titulo.classList.toggle("titulo-branco");
+function changeStyle(){
+    if(footer.style.color !== "rgb(0, 0, 0)"){
+        footer.style.color = "rgb(0, 0, 0)";
+        footer.style.backgroundColor = "rgb(255, 228, 196)"
+    }else{
+        footer.style.color = "rgb(255, 255, 255)";
+        footer.style.backgroundColor = "rgb(66, 66, 66)"
+    }
+}
 
+function changeText(){
     if(titulo.textContent !== "Light Mode: OFF"){
         titulo.textContent = "Light Mode: OFF";
-    }else if(titulo.textContent === "Light Mode: OFF"){
+    }else{
         titulo.textContent = "Light Mode: ON";
     }
-});
+
+    if(botao.textContent !== "Light Mode"){
+        botao.innerHTML = "Light Mode"
+    }else{
+        botao.innerHTML = "Dark Mode";
+    }
+}
