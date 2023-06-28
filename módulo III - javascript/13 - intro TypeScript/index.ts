356d7c887ae3b3f4d0b24ca24ba7eea6a3c98c20
Fenixherm <guibah78@gmail.com>
2023-06-28 17:14:37 -0300

    ts

--- módulo III - javascript/13 - intro TypeScript/index.ts
+++ módulo III - javascript/13 - intro TypeScript/index.ts
@@ -6,3 +6,14 @@ input.addEventListener('input', (event) => {
     const i = event.currentTarget as HTMLInputElement;
     console.log(i.value);
 });
+
+//generic types
+// T é usado para quando tu não sabe o tipo que você quer.
+function adicionaApendiceALista<T>(array: T[], valor: T){
+    return array.map(() => valor);
+}
+
+adicionaApendiceALista([1,2,3], 'd'); 
+//Ele identifica quando um tipo não corresponde ao outro, sem precisar de verificações.
+
+
